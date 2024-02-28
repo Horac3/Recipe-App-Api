705924eaa4a690fa8898eb456e2f2103faaf4cc0
Horac3 <lwandahorace@gmail.com>
2024-02-28 11:16:45 +0200

    Done with user models

--- app/app/settings.py
+++ app/app/settings.py
@@ -81,9 +81,9 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
-         
-        
-        
+
+
+
     }
 }
 
@@ -130,3 +130,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
